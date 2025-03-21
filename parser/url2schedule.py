69311a32997eee5b3c90a585dfb1b76fabc49b07
mikhnenko <mikhnenko@yandex-team.com>
2025-03-21 12:50:25 +0300

    Add links autoupdate

--- parser/url2schedule.py
+++ parser/url2schedule.py
@@ -4,17 +4,7 @@ import xlrd
 import logging
 import json
 
-urls = [
-    'https://misis.ru/files/-/7d25c03a7104eef05c070497a0102925/140225_gi.xls',
-    'https://misis.ru/files/-/180708c8a2a7dd621a10142628cd321e/140225_ibmi.xls',
-#    'https://misis.ru/files/-/a68f9212d6db71b44115abddbfb5e71b/140225_ibo.xlsx', TODO: xls with bad format
-    'https://misis.ru/files/-/75da0398c2654e2a4a3bbba5f203935f/140225_ieu.xls',
-    'https://misis.ru/files/-/9e080c766c3e47bd62c1e0311dbad190/140225_ifki.xls',
-    'https://misis.ru/files/-/40ee7e88bcb71d176a4cd829f91b81c3/140225_ikn.xls',
-    'https://misis.ru/files/-/c57b93865ae04c54a92abf2b5335ab7e/140225_inm.xls',
-    'https://misis.ru/files/-/e5bc8211c013c17eab64e8071cce418a/140225_it.xls',
-    'https://misis.ru/files/-/8b077073a7c38f58d737451e79eb5fbd/140225_pish.xls',
-]
+from get_urls import getNewUrl
 
 local_filename = 'tmp.xls'
 
@@ -157,5 +147,9 @@ def download_file(url, filename):
         print(f"Не удалось скачать файл. Код статуса: {response.status_code}")
 
 
-for url in urls:
-    download_file(url, local_filename)
+if __name__ == "__main__":
+    for url in getNewUrl():
+        if url.endswith('.xlsx'):
+            Log.warning(f'Ignore .xlxs: {url}')
+            continue
+        download_file(url, local_filename)
