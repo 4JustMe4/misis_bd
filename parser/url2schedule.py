4540e0662d4ecca98a246786e38c9ecab445bc3f
mikhnenko <mikhnenko@yandex-team.com>
2025-03-26 18:31:13 +0300

    sort numbers

--- parser/url2schedule.py
+++ parser/url2schedule.py
@@ -233,12 +233,12 @@ if __name__ == "__main__":
         schedule |= downloadFile(url)
 
     with open('group.json', 'w') as f:
-        json.dump(schedule, f, indent=2, ensure_ascii=False)
+        json.dump(schedule, f, indent=2, ensure_ascii=False, sort_keys=True)
 
     byLocation = iterThrow(schedule, updLocation)
     with open('location.json', 'w') as f:
-        json.dump(byLocation, f, indent=2, ensure_ascii=False)
+        json.dump(byLocation, f, indent=2, ensure_ascii=False, sort_keys=True)
 
     byTeacher = iterThrow(schedule, updTeacher)
     with open('teacher.json', 'w') as f:
-        json.dump(byTeacher, f, indent=2, ensure_ascii=False)
+        json.dump(byTeacher, f, indent=2, ensure_ascii=False, sort_keys=True)
