4dd54a4694c36ad2b4a5add1dddee81ab52adec2
mikhnenko <mikhnenko@yandex-team.com>
2025-05-05 13:21:07 +0300

    add function for loading from mongo

--- parser/mongo.py
+++ parser/mongo.py
@@ -8,16 +8,21 @@ MongoLog = getFormattedLogger("mongo")
 TABLE_NAME = 'schedule'
 setupMongoEnv()
 
-def updateMongo(schedule, session, location, teacher):
+def getCollection():
     MongoLog.info(f"Try to connect with monga")
-    try:
-        username = os.environ['MONGO_INITDB_ROOT_USERNAME']
-        password = os.environ['MONGO_INITDB_ROOT_PASSWORD']
-        url = f'mongodb://{username}:{password}@localhost:27017/{TABLE_NAME}'
-        client = pymongo.MongoClient(url)
-        db = client[TABLE_NAME]
-        collection = db['schedule']
+    username = os.environ['MONGO_INITDB_ROOT_USERNAME']
+    password = os.environ['MONGO_INITDB_ROOT_PASSWORD']
+    url = f'mongodb://{username}:{password}@localhost:27017/{TABLE_NAME}'
+    client = pymongo.MongoClient(url)
+    db = client[TABLE_NAME]
+    collection = db['schedule']
+    return collection
 
+
+def updateMongo(schedule, session, location, teacher):
+    MongoLog.info(f"Try to update monga")
+    try:
+        collection = getCollection()
         for name, json in [
             ({"json_name": "schedule"}, schedule),
             ({"json_name": "session"}, session),
@@ -28,4 +33,22 @@ def updateMongo(schedule, session, location, teacher):
             result = collection.update_one(name, {"$set": {"data": json, **name }}, upsert=True)
             MongoLog.info(f"The result of updating is {result}")
     except Exception as error:
-        MongoLog.error(f"Can't connect to mongo {error}, {url}")
+        MongoLog.error(f"Can't update mongo {error}")
+
+
+# sample loadJsonFromMongo('schedule')
+def loadJsonFromMongo(name):
+    MongoLog.info(f"Try to load {name} from mongo")
+    try:
+        query = {"json_name": f"{name}"}
+        collection = getCollection()
+        record = collection.find_one(query)
+        if record:
+            MongoLog.info(f"Info for {name} was found in mongo")
+            return record["data"]
+        else:
+            MongoLog.warning(f"Info for {name} was NOT found in mongo")
+            return None
+    except Exception as error:
+        MongoLog.error(f"Can't load {name} from mongo: {error}")
+        return None
